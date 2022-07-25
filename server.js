8ea29e48e12532618cb09ff345eeb2f84cb6bc78
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-25 13:22:07 -0700

    setting paths

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-
+//setting path
+const path = require('path');
+//requring fs module
 const fs = require("fs")
+//setting port
 const PORT = process.env.PORT || 8080
 
+// requiring the express module
 const express = require("express")
 const app = express()
 
-
-
-
-
-
-
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
