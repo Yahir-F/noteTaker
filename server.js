fce0093419aef2a206892ee3f94a76fe785fad07
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-25 13:50:58 -0700

    setting more routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //setting path
 const path = require('path');
 //requring fs module
+const noteData = require('./db/db.json');
 const fs = require("fs")
 //setting port
 const PORT = process.env.PORT || 8080
@@ -13,6 +14,16 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + './public/index.html'));
+});
 
 
 
