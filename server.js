3106e1527a615ebce6310804db689ac02138a14c
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-25 13:11:53 -0700

    Created port and required mod

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const fs = require("fs")
+const PORT = process.env.PORT || 8080
+
+const express = require("express")
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
