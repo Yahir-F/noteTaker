d636245723ba5c6a0e176fbd5edda49f78479cde
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-25 19:22:05 -0700

    bug fixes

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-//setting path
-const path = require('path');
-//requring fs module
-const noteData = require('./db/db.json');
-const fs = require("fs")
 //setting port
 const PORT = process.env.PORT || 8080
+const fs = require("fs")
 // requiring the express module
 const express = require("express")
 const app = express()
+//setting path
+const path = require('path');
+//requring fs module
+const noteConfig = require('./db/db.json');
 
-app.use(express.static('public'));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
 
 app.get('/api/notes', (req, res) => {
-    res.json(noteData);
+    res.json(noteConfig.slice(1));
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
@@ -31,43 +33,42 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + './public/index.html'));
 });
 //creating function to create a new note
-function addNewNote(noteBody, noteData) {
+function addNewNote(noteBody, notesData) {
     const noteNew = noteBody;
-    if(!Array.isArray(noteData)) 
-        noteData = [];
+    if(!Array.isArray(notesData)) 
+        notesData = [];
 
-        if(noteData.length === 0) {
-            noteData.push(newNote);
+        if(notesData.length === 0) {
+            notesData.push(0);
         }
-    noteBody.id = noteData[0]
-    noteBody[0]++;
-    noteData.push(newNote);
+    noteBody.id = notesData[0]
+    notesData[0]++;
 
-    
+    notesData.push(noteNew);
     fs.writeFileSync(
         path.join(__dirname, "db/db.json"),
-        JSON.stringify(noteData, null)
+        JSON.stringify(notesData, null)
     );
     return noteNew
 }
 //route
 app.post('/api/notes', (req, res) => {
-    const noteNew = addNewNote(req.body, noteData);
+    const noteNew = addNewNote(req.body, noteConfig);
     res.json(noteNew);
 });
 //remove note function 
-function noteRemove(id, noteData) {
+function noteRemove(id, notesData) {
    
-    for(let i = 0; i < noteData.length; i++) {
+    for(let i = 0; i < notesData.length; i++) {
      
-        let info = noteData[i];
+        let info = notesData[i];
         if(info.id === id) {
-            noteData.splice(i, 1);
+            notesData.splice(i, 1);
         
             fs.writeFileSync(
                 path.join(__dirname, "db/db.json"),
       
-                JSON.stringify(noteData, null)
+                JSON.stringify(notesData, null)
             );
             break;
         }
@@ -79,12 +80,9 @@ function noteRemove(id, noteData) {
 
 
 app.delete('/api/notes/:id', (req, res) => {
-    noteRemove(req.params.id, noteData);
-    res.json(noteData);
+    noteRemove(req.params.id, noteConfig);
+    res.json(true);
 });
-
-
-
 
 
 app.listen(PORT, () => {
