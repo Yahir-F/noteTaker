aa25ef5732cde775aaa781e766157fd90e32f7e7
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-25 18:16:37 -0700

    re organized improper routes

--- server.js
+++ server.js
@@ -13,20 +13,22 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
+
 
 app.get('/api/notes', (req, res) => {
     res.json(noteData);
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + './public/index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + './public/index.html'));
 });
 //creating function to create a new note
 function addNewNote(noteBody, noteData) {
